51fa03235202cbf28374b285097c757276b88c48
Alli94629 <al.aracena@duocuc.cl>
2023-05-11 16:05:36 -0400

    final

--- paginas/contacto.js
+++ paginas/contacto.js
@@ -1,20 +1,58 @@
+// const btn = document.getElementById('button');
+
+// document.getElementById('form')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+//    btn.value = 'Sending...';
+
+//    const serviceID = 'default_service';
+//    const templateID = 'template_5hx8wtj';
+
+//    emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//       btn.value = 'Send Email';
+//       alert('Sent!');
+//     }, (err) => {
+//       btn.value = 'Send Email';
+//       alert(JSON.stringify(err));
+//     });
+// });
+
 const btn = document.getElementById('button');
+const emailInput = document.getElementById('email_id');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+function validateEmail() {
+  if (!emailRegex.test(emailInput.value)) {
+    alert('Por favor, introduce un correo electrónico válido.');
+    return false;
+  } else {
+    alert('Correo electrónico válido!');
+    return true;
+  }
+}
 
-   btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_5hx8wtj';
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
+  if (!validateEmail()) {
+  
+    return;
+  }
+
+  btn.value = 'Enviando...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_5hx8wtj';
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+     btn.value = 'Send Email';
+     alert('Enviado!');
+   }, (err) => {
+     btn.value = 'Send Email';
+     alert(JSON.stringify(err));
+   });
 });
