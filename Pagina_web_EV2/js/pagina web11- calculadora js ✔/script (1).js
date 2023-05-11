2c1a44115299ff1139ca2742458cab70ae173341
Alli94629 <al.aracena@duocuc.cl>
2023-05-11 01:25:27 -0400

    orden de carpetas

--- Pagina_web_EV2/js/pagina web11- calculadora js ✔/script (1).js
+++
@@ -1,8 +0,0 @@
-function sumar() {
-    var v1 = parseInt(document.getElementById("valor1").value); //declaro las variables con el id
-    var v2 = parseInt(document.getElementById("valor2").value); //el parseInt permite la suma de numeros
-    var suma=0; //iniciara con valor 0
-
-    suma = v1+v2; //la suma sera el conjunto de sumar las dos variables declaradas
-    document.getElementById("resultado").value=suma; //resultado de la variable suma
-    }
